10fe900b49c9eb7e9c185a27185318d5fb78b169
ANSH KAUSHIK <anshkaushik2007@gmail.com>
2026-07-17 04:02:28 +0530

    Day 2: variables, input/output and arithmetic in C++

---
+++ day2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+ using namespace std;
+ int main(){
+   // int a =5;
+    //int b =10;
+    int a=5,b=7;
+    float pi=3.14;
+    char c ='s';
+    bool truth='true';
+
+    cout<<"this is day2 here the value of a is :" <<a<<".here the value of b is :" << b ;
+   
+    cout<<"this is the value of pi " <<pi ;
+    cout<<"this is the value of char " <<c ;
+    cout<<"this is the bool " <<truth ;
+    return 0;
+
+
+ }
---
+++ day2b.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+ int main()
+ {
+    int num1,num2;
+    
+     cout<<"Enter the value of num1 \n"; // insertion operator
+     cin>>num1; //extraction operator
+    
+    
+     cout<<"Enter the value of num2 \n"; 
+     cin>>num2; 
+  
+     cout<<"the sum is:"<<num1+num2;
+
+
+
+
+   return 0;
+}
