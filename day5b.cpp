66622daf403ae1b3b5590f90003d731a3da5c064
ANSH KAUSHIK <anshkaushik2007@gmail.com>
2026-07-22 08:29:25 +0530

    Add day5b.cpp

---
+++ day5b.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std; 
+  int main(){
+  // loops in C++
+ // 3 types of loops in C++
+ // a. for loop
+// b. while loop
+// c. do while loop
+
+
+// 1 for loop
+// int i=1;
+ //cout<<i;
+ //i++;
+// syntax of for loop 
+ //for (intialization;condition;updation){
+    //body of loop
+ //}
+ //for (int i=1;i<40;i++){
+ //   cout<<i<<endl;
+
+
+ //}
+
+
+// while loop in C++
+// while (condition){
+//     body of loop
+// }
+ 
+//int i=0;
+//while(i<=40)
+//{
+//cout<<i<<endl;
+//i++;
+
+//}
+
+//int i =6;
+//  do {
+    // cout<<i<<endl;
+    // i++;
+// }while(i<=30);
+int i=1;
+do {
+    cout<<i*6<<endl;
+    i++;
+}while(i<=10);
+
+
+ return 0;
+
+}
