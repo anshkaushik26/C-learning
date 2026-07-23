49808e509fc434ccc53edbdf524e00451297299a
ANSH KAUSHIK <anshkaushik2007@gmail.com>
2026-07-23 17:35:34 +0530

    Add day6a.cpp

---
+++ day6a.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+ int i=0;
+//for(i=0;i<5;i++){
+
+   //cout<<i<<endl;
+   // if(i==3){
+    //    break;
+   // }
+ for(i=0;i<10;i++){
+
+    cout<<i<<endl;
+    if(i==3){
+        continue;
+    }
+
+      
+}
+
+
+
+  return 0;
+}
