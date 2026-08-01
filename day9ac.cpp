1f63687d1ec21336e7e9441f94bbc6e7b92bbb43
ANSH KAUSHIK <anshkaushik2007@gmail.com>
2026-08-02 00:26:04 +0530

    Add day9.accpp

---
+++ day9ac.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+using namespace std;    
+
+int main(){
+
+
+// square patter digits    
+//  int n;
+//  cout<<"enter the value of n"<<endl;
+//  cin>>n;
+//  for(int i=1;i<=n;i++){
+//   for(int j=1;j<=n;j++){
+//    cout<<"*";
+//   }
+//   cout<<endl;
+//}
+
+
+
+// star square pattern
+// int n;
+//  cout<<"enter the value of n"<<endl;
+//  cin>>n;
+//  for(int i=0;i<=n-1;i++){
+//   for(int j=0;j<=n-1;j++){
+//    cout<<"*";
+//   }
+//   cout<<endl;
+//
+//  }
+
+ 
+
+//int n =4 ;
+//  
+//  for(int i=1;i<=n;i++){
+//    char ch = 'A';
+//   for(int j=1;j<=n;j++){
+//
+//    cout<<ch;
+//    ch=ch+1;
+//
+//   }
+//
+//   cout<<endl;
+//
+//  }
+
+//int n=3 ;
+//int  num=1;
+//  for(int i=1;i<=n;i++){
+//   
+//   for(int j=1;j<=n;j++){
+//    cout<<num;
+//    num++;
+//    
+//    
+//
+//  }
+//
+//   cout<<endl;
+//
+// }
+
+
+int n =4 ;
+  char ch = 'A';
+  for(int i=0;i<=n;i++){
+    
+   for(int j=0;j<=n;j++){
+    cout<<ch;
+    ch=ch+1;
+
+   }
+
+   cout<<endl;
+
+  }
+
+   
+    return 0;
+}
+
+  
+
+   
+   
