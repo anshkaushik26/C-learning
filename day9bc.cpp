3c4a3ee95f2e6e7536ad7cf59289c601924d0d45
ANSH KAUSHIK <anshkaushik2007@gmail.com>
2026-08-02 00:26:24 +0530

    Add day9.bcpp

---
+++ day9bc.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int n =6 ;
+ 
+  for(int i=0;i<=n-1;i++)//n kitni lines hongi
+  {
+ for(int j=0;j<i+1;j++)// i+1 jo line hai usse ek zyada star hai
+ {
+    cout<<"*";
+    
+   }
+
+   cout<<endl;
+
+  }
+
+   
+    return 0;
+}
+  
